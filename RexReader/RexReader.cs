6ef37cdbc907f3056d5c055cf873aabdfc192c4e
Andrew Varnerin <ffac06@gmail.com>
2013-05-12 14:39:19 -0400

    ReadLayerAsString

---
+++ RexReader/RexReader.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace RexReader {
+    /// <summary>
+    /// Reads a compressed .xp stream and provides methods to read the data
+    /// </summary>
+    public class RexReader {
+        public Stream BaseStream { get; private set; }
+        private Stream Deflated { get; set; }
+        private BinaryReader Reader { get; set; }
+        private int? _layers;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="inputStream"></param>
+        public RexReader(Stream inputStream) {
+            Deflated = new MemoryStream();
+            inputStream.Position = 0;
+            BaseStream = inputStream;
+            using (var deflate = new GZipStream(BaseStream, CompressionMode.Decompress)) {
+                deflate.CopyTo(Deflated);
+            }
+
+            Reader = new BinaryReader(Deflated);
+            Deflated.Position = 0;
+        }
+
+        /// <summary>
+        /// Retrieve the number of layers in the image
+        /// </summary>
+        /// <returns>Number of layers in image</returns>
+        public int GetLayerCount() {
+            if (_layers.HasValue) {
+                return _layers.Value;
+            }
+
+            
+            int layerCount = Reader.ReadInt32();
+            Deflated.Position = 0;
+            _layers = layerCount;
+            return layerCount;
+        }
+
+        /// <summary>
+        /// Gets the width of the layer specified. Throws 
+        /// </summary>
+        /// <param name="layer">The 0-based layer number</param>
+        /// <returns>The width in cells of the specified layer</returns>
+        public int GetLayerWidth(int layer) {
+            if (layer < 0 || layer >= GetLayerCount())
+                throw new ArgumentOutOfRangeException("layer");
+            var offset = (32 + layer * 64) / 8;
+
+            Deflated.Seek(offset, SeekOrigin.Begin);
+            var width = Reader.ReadInt32();
+            Deflated.Seek(0, SeekOrigin.Begin);
+            return width;
+        }
+
+        /// <summary>
+        /// Gets the height of the layer specified. Throws 
+        /// </summary>
+        /// <param name="layer">The 0-based layer number</param>
+        /// <returns>The height in cells of the specified layer</returns>
+        public int GetLayerHeight(int layer) {
+            if (layer < 0 || layer >= GetLayerCount())
+                throw new ArgumentOutOfRangeException("layer");
+            var offset = (32 + 32 + layer * 64) / 8;
+
+            Deflated.Seek(offset, SeekOrigin.Begin);
+            var height = Reader.ReadInt32();
+            Deflated.Seek(0, SeekOrigin.Begin);
+            return height;
+        }
+
+        /// <summary>
+        /// Reads the characters of the specified layer into a string and returns it. No newlines/colors are included.
+        /// Returned string goes down, then right (down each column and wraps up to the top on a new column).
+        /// </summary>
+        /// <param name="layer"></param>
+        /// <returns></returns>
+        public string ReadLayerAsString(int layer) {
+            if (layer < 0 || layer >= GetLayerCount())
+                throw new ArgumentOutOfRangeException("layer");
+
+            var count = GetLayerWidth(layer) * GetLayerHeight(layer);
+            var builder = new StringBuilder(count);
+            var offset = (32 + GetLayerCount() * 64) / 8;
+            Deflated.Seek(offset, SeekOrigin.Begin);
+            const int charColorSize = 6;
+            for (var i = 0; i < count; i++) {
+                builder.Append((char)Reader.ReadInt32());
+                Deflated.Seek(charColorSize, SeekOrigin.Current);
+            }
+            Deflated.Seek(0, SeekOrigin.Begin);
+            return builder.ToString();
+        }
+
+    }
+}
---
+++ RexReaderTests/RexReaderTest.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RexReader;
+namespace RexReaderTests {
+    [TestFixture]
+    public class RexReaderTest {
+
+        private const string SingleLayer = "data\\singleLayer.xp";
+        private const string SingleLayerString = "uA                                                                                                 e";
+        private const int SingleLayerWidth = 10;
+        private const int SingleLayerHeight = 10;
+        private const int SingleLayerLayerCount = 1;
+        private Stream SingleLayerStream { get; set; }
+        public RexReader.RexReader SingleReader { get { return new RexReader.RexReader(SingleLayerStream); } }
+
+        [SetUp]
+        public void SetupStream() {
+            var memoryStream = new MemoryStream();
+            using (var filestream = new FileStream(SingleLayer, FileMode.Open)) {
+                filestream.CopyTo(memoryStream);
+            }
+            SingleLayerStream = memoryStream;
+        }
+
+        [Test]
+        public void New_RexReader_Saves_Stream() {
+            Assert.That(SingleReader.BaseStream, Is.EqualTo(SingleLayerStream));
+        }
+
+        [Test]
+        public void GetLayerCount_With_Single_Layer_Returns_1() {
+            Assert.That(SingleReader.GetLayerCount(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void GetLayerWidth_Throws_ArgumentOutOfRangeException_For_Negative_Layer() {
+            Assert.That(() => SingleReader.GetLayerWidth(-1), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        public void GetLayerWidth_Throws_ArgumentOutOfRangeException_For_Layer_Greater_Than_Layer_Count() {
+            Assert.That(() => SingleReader.GetLayerWidth(4), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        public void GetLayerWidth_Works_For_Single_Layer() {
+            Assert.That(SingleReader.GetLayerWidth(0), Is.EqualTo(SingleLayerWidth));
+        }
+
+        [Test]
+        public void GetLayerHeight_Throws_ArgumentOutOfRangeException_For_Negative_Layer() {
+            Assert.That(() => SingleReader.GetLayerHeight(-1), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        public void GetLayerHeight_Throws_ArgumentOutOfRangeException_For_Layer_Greater_Than_Layer_Count() {
+            Assert.That(() => SingleReader.GetLayerHeight(4), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        public void GetLayerHeight_Works_For_Single_Layer() {
+            Assert.That(SingleReader.GetLayerHeight(0), Is.EqualTo(SingleLayerHeight));
+        }
+
+        [Test]
+        public void ReadLayerAsString_Throws_ArgumentOutOfRangeException_For_Negative_Layer() {
+            Assert.That(() => SingleReader.ReadLayerAsString(-1), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        public void ReadLayerAsString_Throws_ArgumentOutOfRangeException_For_Layer_Greater_Than_Layer_Count() {
+            Assert.That(() => SingleReader.ReadLayerAsString(1), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        public void ReadLayerAsString_Works_For_Single_Layer() {
+            Assert.That(SingleReader.ReadLayerAsString(0), Is.EqualTo(SingleLayerString));
+        }
+
+    }
+}
